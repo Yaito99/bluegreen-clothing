72d9604da30fcc96051b43d3615532590993409d
Yaito <44159867+Yaito99@users.noreply.github.com>
2020-10-15 19:38:01 +0200

    added signup and signin

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -6,13 +6,36 @@ const config = {
     apiKey: "AIzaSyAMULAuobRGe9lKJLGru2ow9QTWOYpgfTw",
     authDomain: "e-bluegreen.firebaseapp.com",
     databaseURL: "https://e-bluegreen.firebaseio.com",
-    projectId: "e-bluegreen",
+    projectId: "e-bluegreen", 
     storageBucket: "e-bluegreen.appspot.com",
     messagingSenderId: "314071580132",
     appId: "1:314071580132:web:5a1c14ab43ce919d92dbf1"
   };
 
+  export const createUserProfileDocument = async(userAuth, data) =>  {
+    if (!userAuth) return;
 
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot= await userRef.get(); 
+    
+    if(!snapShot.exists){
+      const { displayName , email } = userAuth;
+      const createdAt = new Date();
+
+      try {
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...data
+        });
+      }catch(error){
+        console.log('error creating user' , error.message)
+      }
+    }
+    return userRef;
+  }
 
   firebase.initializeApp(config)
 
